7f27c703c704380ab4c133b191c807db81188d71
Jolan <jolanr10@hotmail.com>
2018-08-15 02:59:05 +0300

    Update faucethub.js

--- faucethub.js
+++ faucethub.js
@@ -1,3 +1,7 @@
-$('div.row.dice-auto-content-holder.dice-content-holder > div :input').each(function(){
-	$(this).removeAttr('disabled');
+$(function(){
+	$('div.row.dice-auto-content-holder.dice-content-holder > div :input').each(function(){
+		$(this).removeAttr('disabled');
+	});
+	$('#dice-manual-auto-btns > label:nth-child(2) > span.label.label-danger').remove();
+	$('#dice-manual-auto-btns > label:nth-child(2)').trigger('click');
 });
